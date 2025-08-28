7c06ab1d9f40c1ed9a98c58ca90e427fffcd54f1
shrijana thapa magar <67953731+shrijana-thapa@users.noreply.github.com>
2025-08-28 08:22:34 -0700

    done ecommerce unit testing

--- src/app/ecommerce/ecommerce/ecommerce.spec.ts
+++ src/app/ecommerce/ecommerce/ecommerce.spec.ts
@@ -4,6 +4,9 @@ import { EcommerceComponent } from './ecommerce';
 import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { ProductState } from '../../store/product.state';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import * as ProductActions from '../../store/product.actions';
+import { Product } from '../../store/product.model';
+import { By } from '@angular/platform-browser';
 
 
 describe('Ecommerce', () => {
@@ -26,7 +29,7 @@ describe('Ecommerce', () => {
       providers:[provideMockStore({initialState:{product:initialState}})]
     })
     .compileComponents();
-
+    store=TestBed.inject(MockStore);
     fixture = TestBed.createComponent(EcommerceComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -34,5 +37,29 @@ describe('Ecommerce', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should dispatch productload action when on init ',()=>{
+
+   const dispatchSpy=spyOn(store,'dispatch');
+   expect(dispatchSpy).toHaveBeenCalledWith(ProductActions.loadProducts());
+  });
+
+    it('should dispatch addToCart when addToCart() is called', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+    const product: Product = { id: 2, title: 'Laptop', price: 1000, description: 'Gaming laptop',category:'Electronics', image: 'laptop.jpg' };
+    component.addToCart(product);
+    expect(dispatchSpy).toHaveBeenCalledWith(ProductActions.addToCart({ product }));
+  });
+    it('should dispatch removeFromCart when removeFromCart() is called', () => {
+    const dispatchSpy = spyOn(store, 'dispatch');
+    component.removeFromCart(2);
+    expect(dispatchSpy).toHaveBeenCalledWith(ProductActions.removeFromCart({ productId: 2 }));
+  });
+
+    it('should show cart count in button', () => {
+    fixture.detectChanges();
+    const button = fixture.debugElement.query(By.css('button[color="accent"]')).nativeElement;
+    expect(button.textContent).toContain('Cart (0)');
   });
 });
